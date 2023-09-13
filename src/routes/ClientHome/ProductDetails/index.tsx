e1f1067b660a3dfafef7a21b77ea2f46d7b1e4d5
alison <adevpr11@gmail.com>
2023-09-13 18:52:57 -0300

    Detalhes do produto dinâmico com parâmetro de rota

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,26 +2,20 @@ import './styles.css';
 import ButtonInverse from '../../../components/ButtonInverse';
 import ButtonPrimary from '../../../components/ButtonPrimary';
 import ProductDetailsCard from '../../../components/ProductDetailsCard';
-import { ProductDTO } from '../../../models/product';
+import * as productService from '../../../services/product-service';
+import { useParams } from 'react-router-dom';
 
-const product: ProductDTO = {
-  id: 2,
-  name: "SmartTv",
-  description: "TV de última geração",
-  imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
-  price: 2500.99,
-  categories: [
-    { id: 2, name: 'Eletrônicos' },
-    { id: 3, name: 'Computadores' },
-    { id: 4, name: 'Importados' }
-  ]
-}
 
 export default function ProductDetails() {
+  const params = useParams();
+  const product = productService.findById(Number(params.productId));
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        <ProductDetailsCard product={product}></ProductDetailsCard>
+        {
+          product && <ProductDetailsCard product={product} />
+        }
         <div className="dsc-btn-page-container">
           <ButtonPrimary name="Comprar"></ButtonPrimary>
           <ButtonInverse name="Início"></ButtonInverse>
