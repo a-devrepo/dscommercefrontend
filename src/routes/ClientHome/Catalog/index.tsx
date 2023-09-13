dbd43c2563274d578cbe7541304df3c0d2823dad
alison <adevpr11@gmail.com>
2023-09-13 16:46:32 -0300

    Renderizando catálogo de produtos dinamicamente

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -1,24 +1,8 @@
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import CatalogCard from '../../../components/CatalogCard';
 import SearchBar from '../../../components/SearchBar';
-import { ProductDTO } from '../../../models/product';
 import './styles.css';
-
-
-
-
-const product: ProductDTO = {
-  id: 2,
-  name: "SmartTv",
-  description: "TV de última geração",
-  imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
-  price: 2500.99,
-  categories: [
-    { id: 2, name: 'Eletrônicos' },
-    { id: 3, name: 'Computadores' },
-    { id: 4, name: 'Importados' }
-  ]
-}
+import * as productService from '../../../services/product-service';
 
 export default function Catalog() {
   return (
@@ -26,18 +10,9 @@ export default function Catalog() {
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
-          <CatalogCard product={product}></CatalogCard>
+          {
+            productService.findAll().map(product => <CatalogCard key={product.id} product={product} />)
+          }
         </div>
         <ButtonNextPage></ButtonNextPage>
       </section>
