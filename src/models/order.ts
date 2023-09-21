00f7ea5f371157da3b71763d4fde407f91a9cae5
alison <adevpr11@gmail.com>
2023-09-21 16:45:59 -0300

    Mensagem de carrinho vazio

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,7 +1,7 @@
 export class OrderDTO {
   id?: number;
   items: OrderItemDTO[] = [];
-  getTotal(): number {
+  get total(): number {
     let sum = 0;
     this.items.forEach((item) => {
       sum += item.subTotal;
