fdf296ac65849086bc4275303423d6705f8003de
alison <adevpr11@gmail.com>
2023-09-22 09:32:37 -0300

    Implementando o tratamento de evento onSearch

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import './styles.css';
 
-export default function SearchBar() {
+type Props = {
+    onSearch: Function;
+}
+
+
+export default function SearchBar({ onSearch }: Props) {
+
+    const [text, setText] = useState("");
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        onSearch(text);
+    }
+
+    function handleChange(event: any) {
+        setText(event.target.value);
+    }
+
+    function handleResetClick(event: any) {
+        setText("");
+        onSearch(text);
+    }
+
     return (
-        <form className="dsc-search-bar">
+        <form className="dsc-search-bar" onSubmit={handleSubmit}>
             <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset">&#x2715;</button>
+            <input value={text} onChange={handleChange} type="text" placeholder="Nome do produto" />
+            <button onClick={handleResetClick}>&#x2715;</button>
         </form>);
 }
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,30 +5,31 @@ import './styles.css';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product';
 import * as productService from '../../../services/product-service';
-import { CategoryDTO } from '../../../models/category';
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<ProductDTO[]>();
+  const [productName, setProductName] = useState("");
 
-  const objTest: CategoryDTO = {
-    id: 8,
-    name: 'jardinagem',
+
+  function handleSearch(searchText: string) {
+    setProductName(searchText);
   }
 
   useEffect(() => {
-    productService.findPageRequest(0, "ma")
+    productService.findPageRequest(0, productName)
       .then(response => {
         setProducts(response.data.content)
       }).
       catch(error => {
 
       });
-  }, [])
+  }, [productName])
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
             products && products.map(product => <CatalogCard key={product.id} product={product} />)
