2c606d88a7019e365d6e8bc3be9a7ecddb23f28b
alison <adevpr11@gmail.com>
2023-09-29 10:18:07 -0300

    Mostrando erro global no formulário de login

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -13,6 +13,8 @@ export default function Login() {
 
     const navigate = useNavigate();
 
+    const [submitResponseFail, setSubmitResponseFail] = useState(false);
+
     const [formData, setFormData] = useState<any>({
         username: {
             value: "",
@@ -36,7 +38,15 @@ export default function Login() {
 
     function handleSubmit(event: any) {
         event.preventDefault();
-        console.log(forms.toValues(formData));
+
+        setSubmitResponseFail(false);
+
+        const formDataValidated = forms.dirtyAndValidateAll(formData);
+        if (forms.hasAnyInvalid(formDataValidated)) {
+            setFormData(formDataValidated);
+            return;
+        }
+
         authService.loginRequest(forms.toValues(formData))
             .then(response => {
                 if (response) {
@@ -46,7 +56,7 @@ export default function Login() {
                 }
             })
             .catch(error => {
-                console.log("Erro no login", error);
+                setSubmitResponseFail(true);
             });
     }
 
@@ -83,6 +93,13 @@ export default function Login() {
                                 onChange={handleInputChange} />
                         </div>
                     </div>
+                    {
+                        submitResponseFail &&
+                        <div className='dsc-form-global-error'>
+                            Usuário ou senha inválidos
+                        </div>
+                    }
+
                     <div className="dsc-mt20 dsc-login-form-buttons">
                         <button type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
                     </div>
